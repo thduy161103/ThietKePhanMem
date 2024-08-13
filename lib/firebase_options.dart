b038b2a7d0df34b06cdc578f7038997753932448
thduy161103 <147958221+thduy161103@users.noreply.github.com>
2024-08-13 22:12:58 +0700

    login firebase

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return FirebaseOptions(
+      apiKey: 'AIzaSyD6Ly3VdDnkAbArP_vZfHjAiAafNigQfAo',
+      appId: '1:318307432230:android:defb73e0202c9dd74233e7',
+      messagingSenderId: '318307432230',
+      projectId: 'thietkephanmem-368a3',
+    );
+  }
+}
